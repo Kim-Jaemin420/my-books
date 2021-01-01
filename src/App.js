48ac08c0d374bae0b01eecf8f27ca1b73d0f4a25
Kim-Jaemin420 <jany240@naver.com>
2021-01-01 19:41:50 +0900

    리덕스 관련한 것 모두 주석 처리

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
 // redux
-import { Provider } from 'react-redux';
-import store from './redux/store';
+// import { Provider } from 'react-redux';
+// import store from './redux/store';
 
 function App() {
   return (
     <ErrorBoundary FallbackComponent={Error}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/signin" component={Signin} />
-            <Route path="/" exact component={Home} />
-            <Route component={NotFound} />
-          </Switch>
-        </BrowserRouter>
-      </Provider>
+      {/* <Provider store={store}> */}
+      <BrowserRouter>
+        <Switch>
+          <Route path="/signin" component={Signin} />
+          <Route path="/" exact component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+      {/* </Provider> */}
     </ErrorBoundary>
   );
 }
