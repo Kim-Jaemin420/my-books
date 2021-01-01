aaf586ba772de2c0e3e6faa4176839a0341eb4de
Kim-Jaemin420 <jany240@naver.com>
2021-01-01 20:17:53 +0900

    로딩 화면 구현을 위해 2초 딜레이 걸기 + 로딩 아이콘 추가 + books 배열에 데이터가 없다면 '데이터 없습니다.' 렌더링

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,21 +2,29 @@ import React, { Component } from 'react';
 import axios from "axios";
 import { sleep } from '../utils';
 
+// loading 아이콘
+import { LoadingOutlined } from "@ant-design/icons";
+
 
 class BookList extends Component {
   state = {
     books: [],
+    loading: false,
   };
 
   render() {
-    const { books } = this.state;
+    const { books, loading } = this.state;
 
 
     return (
       <div>
-        {books.map(book => (
-          <li>{book.title}</li>
-        ))}
+        <h1>Book List {loading && <LoadingOutlined />}</h1>
+        {books.length === 0 && <p>데이터가 없습니다.</p>}
+        {books.length !== 0 && (<ul>
+          {books.map(book => (
+            <li>{book.title}</li>
+          ))}
+        </ul>)}
       </div>
     );
   }
@@ -24,6 +32,9 @@ class BookList extends Component {
 
 
   async componentDidMount() {
+    this.setState({ loading: true });
+
+    await sleep(2000);
     // 최초 렌더링시 서버에 책 리스트 요청한다.
     try {
       const response = await axios.get('https://api.marktube.tv/v1/book', {
@@ -32,8 +43,10 @@ class BookList extends Component {
         }
       });
       console.log(response.data);
-      await sleep(2000);
-      this.setState({ books: response.data });
+      this.setState({
+        books: response.data,
+        loading: false,
+      });
 
     } catch (error) {
       console.log(error);
