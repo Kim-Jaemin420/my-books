ab3454346b8bca9e25cd6c1f68e5d9a520f96fc8
Kim-Jaemin420 <jany240@naver.com>
2021-01-04 00:52:19 +0900

    api로 받은 books를 useDispatch를 사용해 dispatch 하였다.

--- src/containers/BookListContainer.jsx
+++ src/containers/BookListContainer.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import BookList from '../components/BookList';
+import { bookSuccess } from '../redux/actions';
 
 const BookListContainer = ({token}) => {
   // redux와의 연결고리
   const books = useSelector(state => state.books.books);
 
-  return <BookList token={token} books={books}/>;
+  // api로 받은 books response data를 리덕스에게 dispatch
+  const dispatch = useDispatch();
+  
+  const setBooks = useCallback(books => {
+    dispatch(bookSuccess(books))},
+    [dispatch]);
+
+  return <BookList token={token} books={books} setBooks={setBooks}/>;
 };
 
 export default BookListContainer;
