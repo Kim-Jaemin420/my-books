4add9c7eaefe2335e490306a2e4c555d3be5b0d8
Kim-Jaemin420 <jany240@naver.com>
2021-01-04 01:12:21 +0900

    loading과 error에 대한 리듀서

--- src/redux/books.js
+++ src/redux/books.js
@@ -1,14 +1,18 @@
-import { BOOK_SUCCESS } from './actions';
+import { BOOK_ERROR, BOOK_START, BOOK_SUCCESS } from './actions';
 
-const initialState = {books: []};
+const initialState = { books: [], loading: false, error: null };
 
 // reducer
 export default function books(prevState = initialState, action) {
- switch (action.type) {
-   case BOOK_SUCCESS:
-     // {books: [...prevState, ...action.books]}라고 안해줘도 되나?
-     return {books: action.books}
-   default:
-     return prevState;
- }
+  switch (action.type) {
+    case BOOK_SUCCESS:
+      // {books: [...prevState, ...action.books]}라고 안해줘도 되나?
+      return { books: action.books, loading: false, error: null };
+    case BOOK_START:
+      return { ...prevState, loading: true, error: null };
+    case BOOK_ERROR:
+      return { ...prevState, loading: false, error: action.books.error };
+    default:
+      return prevState;
+  }
 }
