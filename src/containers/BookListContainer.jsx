60c2cb0d76cf8ecb06da7f0ea50e1f6318c9bfab
Kim-Jaemin420 <jany240@naver.com>
2021-01-04 01:55:53 +0900

    Books 비동기 작업 가져옴

--- src/containers/BookListContainer.jsx
+++ src/containers/BookListContainer.jsx
@@ -1,6 +1,8 @@
 import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import BookList from '../components/BookList';
+import axios from "axios";
+import { sleep } from '../utils';
 import { bookFail, bookStart, bookSuccess } from '../redux/actions';
 
 const BookListContainer = ({token}) => {
@@ -11,18 +13,26 @@ const BookListContainer = ({token}) => {
 
   // api로 받은 books response data를 리덕스에게 dispatch
   const dispatch = useDispatch();
-  
-  const successBooks = useCallback(books => {
-    dispatch(bookSuccess(books))},
-    [dispatch]);
 
-  const startBooks = useCallback(() => {
-    dispatch(bookStart())},
-    [dispatch]);
-  
-    const failBooks = useCallback((error) => {
-      dispatch(bookFail(error))},
-      [dispatch]);
+      const getBooks = useCallback(async () => {
+        try {
+          dispatch(bookStart());
+    
+          await sleep(2000);
+    
+          const response = await axios.get('https://api.marktube.tv/v1/book', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            }
+          });
+    
+          dispatch(bookSuccess(response.data));
+    
+        } catch (error) {
+          console.log(error);
+          dispatch(bookFail(error));
+        }
+      }, [dispatch, token])
 
   return (
     <BookList 
@@ -30,9 +40,7 @@ const BookListContainer = ({token}) => {
     books={books} 
     loading={loading} 
     error={error} 
-    successBooks={successBooks}
-    startBooks={startBooks}
-    failBooks={failBooks}
+    getBooks={getBooks}
     />
   );
 };
