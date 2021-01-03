d0e263c965a62ddc2fe9881ee5690ab71f9ea11e
Kim-Jaemin420 <jany240@naver.com>
2021-01-04 01:08:26 +0900

    loading과 error 액션 타입과 생성자 함수 설정

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,9 +3,20 @@
 
 // action types
 export const BOOK_SUCCESS = 'BOOK_SUCCESS';
+export const BOOK_START = 'BOOK_START';
+export const BOOK_ERROR = 'BOOK_ERROR';
 
 // action creators
 export const bookSuccess = (books) => ({
   type: BOOK_SUCCESS,
   books,
+});
+
+export const bookStart = () => ({
+  type: BOOK_START,
+});
+
+export const bookError = (error) => ({
+  type: BOOK_ERROR,
+  error,
 });
