e115a81ff717ca053624d80c60ebfc423dd98d26
Kim-Jaemin420 <jany240@naver.com>
2021-01-04 16:47:18 +0900

    token을 초기에 로컬스토리지에서 가져오고 싶어서 createStore에 설정

--- src/redux/create.js
+++ src/redux/create.js
@@ -9,8 +9,25 @@ import { createBrowserHistory } from 'history';
 
 export const history = createBrowserHistory();
 
+/*
+새로고침할 때 createStore가 발생한다.
+따라서 새로고침할 때 LocalStorage에서 토큰을 가져와
+리덕스에 있는 data화 시켜주면 된다.
+createStore 인자로 최초 state값을 설정해줄 수 있는데,
+이  최초 값은 모든 리덕스의 state를 합친 모양과 같아야 한다.
+ {books: {books: [], loading: false, error: null} },
+ {auth: {token: [], loading: false, error: null} },
+*/
+/*
+그런데, 모든 state의 초기값을 설정해줄 필요는 없다.
+각각의 모듈의 리듀서에서 초기 state값을 넣어주고 있기 때문에,
+필요한 모듈만 최초값을 넣어준다.
+*/
 const store = createStore(
   reducer,
+  {
+    auth: { token: localStorage.getItem('token'), loading: false, error: null },
+  },
   composeWithDevTools(
     applyMiddleware(thunk.withExtraArgument(history), promise),
   ),
