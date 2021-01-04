fdbc5497c720e76edbbb093a2d1b0f1d53dbca72
Kim-Jaemin420 <jany240@naver.com>
2021-01-04 16:19:55 +0900

    현재 BrowserRouter를 사용중 => Router로 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 
 // pages
@@ -9,19 +9,26 @@ import NotFound from './pages/NotFound';
 
 // redux
 import { Provider } from 'react-redux';
-import store from './redux/create';
+import store, { history } from './redux/create';
 
+/* 
+createBrowserHistory를 사용하면 생기는 문제
+BrowserRouter는 우리가 create한 history를 사용하지 않고
+자체적으로 history를 생성해서 그것을 사용한다.
+따라서 BrowserRouter 대신 Router를 사용하고 우리가 만든
+history를 props로 넘겨준다.
+*/
 function App() {
   return (
     <ErrorBoundary FallbackComponent={Error}>
       <Provider store={store}>
-        <BrowserRouter>
+        <Router history={history}>
           <Switch>
             <Route path="/signin" component={Signin} />
             <Route path="/" exact component={Home} />
             <Route component={NotFound} />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </Provider>
     </ErrorBoundary>
   );
