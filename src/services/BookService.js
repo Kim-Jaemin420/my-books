b3a3a54ea0711b82183f071e4e8f94a995057d3c
Kim-Jaemin420 <jany240@naver.com>
2021-01-04 14:17:51 +0900

    또 다른 api 호출문을 작성. 그런데, api 주소에서 중복이 발생한다.

--- src/services/BookService.js
+++ src/services/BookService.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import axios from 'axios';
 
 class BookService extends Component {
   static async getBooks(token) {
@@ -9,6 +10,17 @@ class BookService extends Component {
     });
 
     return response.data;
+  }
+
+  static async getBook(token, bookId) {
+    const response = await axios.get(
+      `https://api.marktube.tv/v1/book/${bookId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
   }
 }
 
