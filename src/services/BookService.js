cf9b4a80a9c0e0dbd052c578f5d8797ccdfbc5df
Kim-Jaemin420 <jany240@naver.com>
2021-01-04 14:21:11 +0900

    중복되는 api 주소를 변수로 관리

--- src/services/BookService.js
+++ src/services/BookService.js
@@ -1,9 +1,14 @@
 import { Component } from 'react';
 import axios from 'axios';
 
+// 이렇게 변수로 선언하면, 환경에 따라 다르게 설정해줄 수도 있다.
+// 예를 들어, 개발 모드일때는 dev.api.martube
+// production 모드일때는 api.marktube
+const BOOK_API_URL = 'https://api.marktube.tv/v1/book';
+
 class BookService extends Component {
   static async getBooks(token) {
-    const response = await axios.get('https://api.marktube.tv/v1/book', {
+    const response = await axios.get(BOOK_API_URL, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -13,14 +18,13 @@ class BookService extends Component {
   }
 
   static async getBook(token, bookId) {
-    const response = await axios.get(
-      `https://api.marktube.tv/v1/book/${bookId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+    const response = await axios.get(`${BOOK_API_URL}/${bookId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
       },
-    );
+    });
+
+    return response.data;
   }
 }
 
