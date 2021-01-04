eac7d4f8fd516c565e66bec7d774df4cccbe95b2
Kim-Jaemin420 <jany240@naver.com>
2021-01-04 14:01:07 +0900

    비동기 로직을 thunk로 옮김

--- src/containers/BookListContainer.jsx
+++ src/containers/BookListContainer.jsx
@@ -1,9 +1,7 @@
 import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import BookList from '../components/BookList';
-import axios from "axios";
-import { sleep } from '../utils';
-import { bookFail, bookStart, bookSuccess } from '../redux/actions';
+import { bookFail, bookStart, bookSuccess, getBooksThunk } from '../redux/actions';
 
 const BookListContainer = ({token}) => {
   // redux와의 연결고리
@@ -14,25 +12,9 @@ const BookListContainer = ({token}) => {
   // api로 받은 books response data를 리덕스에게 dispatch
   const dispatch = useDispatch();
 
-      const getBooks = useCallback(async () => {
-        try {
-          dispatch(bookStart());
-    
-          await sleep(2000);
-    
-          const response = await axios.get('https://api.marktube.tv/v1/book', {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            }
-          });
-    
-          dispatch(bookSuccess(response.data));
-    
-        } catch (error) {
-          console.log(error);
-          dispatch(bookFail(error));
-        }
-      }, [dispatch, token])
+  const getBooks = useCallback(async () => {
+      dispatch(getBooksThunk(token));
+    }, [dispatch, token])
 
   return (
     <BookList 
